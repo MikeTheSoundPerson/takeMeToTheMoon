4af8a70cd3796c8291f5108e269ae230f7acb155
JonasGruetzmacher <61141274+JonasGruetzmacher@users.noreply.github.com>
2023-03-25 12:08:06 +0100

    npc accepts orbs

--- takemetothemooon/Assets/Scripts/NPC.cs
+++ takemetothemooon/Assets/Scripts/NPC.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using System;
 
 public class NPC : MonoBehaviour
 {
     [SerializeField] private StudioEventEmitter NPCTalk;
 
     public Player player;
+
+    public List<GameObject> wantedOrbs = new List<GameObject>();
+
+    public Action OnRecievedOrbs;
 
 
     public bool talking;
@@ -31,14 +36,35 @@ public class NPC : MonoBehaviour
     {
         if(talking)
         {
-            Debug.Log("Talk to player");
+            if(wantedOrbs.Count == 0)
+            {
+                Debug.Log("No orbs wanted");
+                return;
+            }
+            Orb wantedOrb = wantedOrbs?[0]?.GetComponent<Orb>();
+
+            if(wantedOrb?.name == orb?.name)
+            {
+                
+                wantedOrbs.RemoveAt(0);
+                if(wantedOrbs.Count == 0)
+                {
+                    Debug.Log("All orbs collected");
+                    OnRecievedOrbs?.Invoke();
+                }
+            }
+            else
+            {
+                Debug.Log("Wrong orb");
+            }
+            
+            
         }
     }
 
     void OnEnable()
     {
         player.OnTalk += TalkToPlayer;
-        Debug.Log("test");
     }
 
     void OnDisable()
